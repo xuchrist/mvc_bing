32fa95af9d02035a09f9f3d62a47ff0dcda99e6c
a <a>
2017-05-31 17:45:35 +0800

    mvc 登录，可能缺少数据，需要补�

---
+++ WebPage/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebPage
+{
+    public class MvcApplication : Spring.Web.Mvc.SpringMvcApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
